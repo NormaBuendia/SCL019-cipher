6e2bbc6ba21826f15e1b8fc6d6c5870eb4dcb51a
Norma <>
2021-12-23 10:35:06 -0300

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,64 @@
-const cipher = {
-  // ...
-};
+
+const cipher = {//cipher es un objeto
+
+code: function(text,offSet){ 
+  // En este code a traves de una funcion a la que le pasamos al texto y el desplazamiento
+  let cadena= ""; 
+  //es una variable con un string (usando comillas) vacio
+
+  for (let i=0; i<text.length; i++){
+    //el for no depende del array
+    //bucle o for es un ciclo que se va a repetir todo lo que va entre sus llaves
+    //comprobar si la letra esta en el array de letras,
+    //tomo cada letra del mensaje segun los parametros que se indican.
+  
+  let ascii = text.charCodeAt(i);
+  //obtiene el numero de la letra en el codigo ascii uno por uno de forma independiente 
+  let asciiOffSet = (ascii - 65 + offSet) % 26 + 65;
+  // formula del codigo ascii
+  // obtiene numero o valor del texto segun codigo ascii segun el desplazamiento indicado
+  //console.log(asciiOffSet);
+ 
+  let codedText = String.fromCharCode(asciiOffSet);
+  //debugger
+  //me devuelve una cadena de valores, siempre van juntos string.from.....
+  //console.log(codedText); 
+  cadena+=codedText; 
+  //debugger
+  //console.log(cadena);
+  // sumarle al string vacio, concatena el texto, 
+  }
+  //console.log(cadena);
+
+return cadena;
+//muestra la cadena de texto en la ventana indicada  
+},
+
+decode: function(text, offSet){ //que le pasa al texto y al desplazamiento
+   let cadena = "";
+  //es una variable con un string (usando comillas) vacio
+   for (let i=0; i<text.length; i++){ 
+  //bucle repite una accion varias veces
+  //console.log(text[i]);
+   let ascii = text.charCodeAt(i);
+   //console.log(ascii);
+  //obtine codigo ascii de mensaje
+ //if((ascii>64) && (ascii<91)){
+  // si esta entre este valor y entre este  otro valor
+let asciiOffSet = (ascii + 65 - offSet) % 26 + 65;
+//formula codigo ascii
+//console.log(asciiOffSet);
+let decodedText = String.fromCharCode(asciiOffSet);
+//de numero ascii a cadena de alfabeto
+cadena+=decodedText;
+  // concatena el texto, sumarle al string vacio
+  //}
+}
+  //console.log(array)
+return cadena;
+// retorna el arreglo
+
+}
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//crear usuario y contraseña
+const botonGo = document.getElementById("Go");
+if(botonGo){
+    document.getElementById("Go").addEventListener("click", function login(){
+        //tomar el elemento y cuando haga click pase lo siguiente:
+    let usuario = document.getElementById("Username").value;
+    //sacar el texto que tiene el mensaje y darle el valor
+    let clave = document.getElementById("Password").value;
+    
+    if(usuario == "norma" && clave == "seguro"){
+        //si el usuarios es ... y la clave es ...
+        location.pathname = "src/pagina.html";
+        //ubicacion ruta de la pagina
+    }
+    
+    else alert("Usuario o Password incorrecto")
+    // en otro caso indica el siguiente mensaje....
+    
+    });
+}
+else{
+
+//window.addEventListener("load", inicio, true);
+function mayus(e) {
+    e.target.value = e.target.value.toUpperCase();
+    //el target es el input que tiene el value, donde sucede
+//function inicio (){
+    //document.getElementById("mensaje").addEventListener("keyup", function(){
+     //this.value = this.value.toUpperCase();   
+   // });
+}
+
+document.getElementById("mensaje").addEventListener("keyup",mayus);
+document.getElementById("cifrar").addEventListener("click",function(){ 
+    // tomo el elemento getElmen.. y cuando hago click pasa lo siguiente:
+    let text = document.getElementById("mensaje").value; 
+    //sacar el texto que tiene el mensaje y con value el valor del mensaje
+    let offSet = document.getElementById("desplazamiento").value;
+    // sacar el desplazmiento y obtener su valor
+    let codedText = cipher.code(text,offSet);
+ 
+    let finalText = document.getElementById("mensaje2");
+    finalText.value=codedText;
+    //al final devuelva un texto con dezplazamiento y se escriba en donde dice mensaje2
+},true);
+
+document.getElementById("mensaje2").addEventListener("keyup",mayus);
+    
+    document.getElementById("descifrar").addEventListener("click",function(){
+// tomo el elemento getElmen.. y cuando hago click, captura de eventos:
+   let text = document.getElementById("mensaje").value;
+   //sacar el texto que tiene el mensaje, que es el elemento en concreto y con value el valor del mensaje
+   let offSet = document.getElementById("desplazamiento").value;
+   //let offsetNumber = Number.parseInt(offSet);
+   // sacar el desplazmiento y obtener su valor
+   let decodedText = cipher.decode(text,offSet);
+   //traigo el valor de index.js
+   let finalText = document.getElementById("mensaje2");
+   finalText.value=decodedText;
+    
+},true);}
+//para la frase de burbujeo, de afuera hacia adentro
+
+
